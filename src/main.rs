84b19c4d37919dee0424a852f23a9390f49d60b4
Andy Young <ayscript@gmail.com>
2019-01-11 14:33:24 -0700

    Accessing Array elements

--- src/main.rs
+++ src/main.rs
@@ -76,17 +76,25 @@
 //     println!("The value of x is: {}. y is {}, and z is {}.", x, y, z);
 // }
 
+// fn main() {
+//     let x: (i32, f64, u8) = (500, 6.4, 1);
+
+//     let five_hundred = x.0; // dot notation and indexing to get tuple value
+
+//     let six_point_four = x.1;
+
+//     let one = x.2;
+
+//     println!(
+//         "five_hundred = {}. six_point_four = {}. one = {}.",
+//         five_hundred, six_point_four, one
+//     );
+// }
+
 fn main() {
-    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let a = [1, 2, 3, 4, 5];
+    let first = a[0];
+    let second = a[1];
 
-    let five_hundred = x.0; // dot notation and indexing to get tuple value
-
-    let six_point_four = x.1;
-
-    let one = x.2;
-
-    println!(
-        "five_hundred = {}. six_point_four = {}. one = {}.",
-        five_hundred, six_point_four, one
-    );
+    println!("first = {}. second = {}", first, second);
 }
